fe498ba71d76b0d8ec258c02c926d80a9f016d4a
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-07 02:08:16 +0000

    codificacion de input en toDoList.jsx

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -10,19 +10,16 @@ const ToDoList = () => {
             .then(response => response.json())
             .then((data) => setUser(data))
             .catch((error) => console.log(error))
-
     }
 
     function getTodos() {
         return fetch("https://playground.4geeks.com/todo/users/jcamposg6991", { method: "GET" })
         .then(response => response.json())
         .then((data) => setTodos(data.todos))
-        .catch((error) => console.log(error))
-        
+        .catch((error) => console.log(error)) 
     }
 
     useEffect(() => {
-
         getUser()
         getTodos()
 
@@ -34,13 +31,9 @@ const ToDoList = () => {
         <div className="text-center toDoList">
              <h1>To do List</h1>
             {<h1>Usuario: {user.name}</h1>}
-            {/* <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              onKeyDown={(e) => e.key === 'Enter' && addTask(inputValue)}
-              placeholder="What needs to be done?"
-            /> */}
+            <input type="text" /*value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={(e) => e.key === 'Enter' && addTask(inputValue)}*/
+              placeholder="¿Cual es la tarea a realizar?"
+            />
             <ul>
               {todos.map((item, index) => (
                 <li key={index} className="task-item">
