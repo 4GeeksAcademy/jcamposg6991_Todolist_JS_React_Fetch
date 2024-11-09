ff7b1565e2f69e843e04c9f7f805eb5b13bde96a
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-09 02:48:47 +0000

    codificacion de actualizacion de la lista de tareas

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -1,80 +1,93 @@
 import React, { useState, useEffect } from "react";
 
 const ToDoList = () => {
-
-  const [user, setUser] = useState("")
-  const [todos, setTodos] = useState([])
-  const [inputValue, setInputValue] = useState("")
+  const [user, setUser] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   function createUser() {
-    return fetch("https://playground.4geeks.com/todo/users/jcamposg6991", { method: "POST", headers: { "Content-Type": "application/json" } })
-      .then(response => response.json())
-      .then((data) => console.log(data))
-      .then((error) => console.log(error))
+    fetch("https://playground.4geeks.com/todo/users/jcamposg6991", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (response.status === 400) {
+          return getUser(); // Si el usuario ya existe, cargar datos de usuario
+        }
+        return response.json();
+      })
+      .catch((error) => console.log(error));
   }
 
   function getUser() {
-    fetch("https://playground.4geeks.com/todo/users/jcamposg6991", { method: "GET" })
-      .then(response => {
-        if (response.status === 404) {
-          createUser()
-        }
-        return response.json()
-      })
+    return fetch("https://playground.4geeks.com/todo/users/jcamposg6991", {
+      method: "GET",
+    })
+      .then((response) => response.json())
       .then((data) => setUser(data))
-      .catch((error) => console.log(error))
+      .catch((error) => console.log(error));
   }
 
   function getTodos() {
-    return fetch("https://playground.4geeks.com/todo/users/jcamposg6991", { method: "GET" })
-      .then(response => response.json())
+    return fetch("https://playground.4geeks.com/todo/users/jcamposg6991", {
+      method: "GET",
+    })
+      .then((response) => response.json())
       .then((data) => setTodos(data.todos))
-      .catch((error) => console.log(error))
+      .catch((error) => console.log(error));
   }
 
-  function createTodo(todo){
-    return fetch("https://playground.4geeks.com/todo/todos/jcamposg6991", { method: "POST", headers: { "Content-Type": "application/json" },   body: JSON.stringify({
-      label: todo,
-      is_done: "false"
-    }) })
-    .then(response => response.json())
-    .then((data) => console.log(data))
-    .then((error) => console.log(error))
-    
+  function createTodo(todo) {
+    if (todo.trim() !== "") {
+      return fetch("https://playground.4geeks.com/todo/todos/jcamposg6991", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          label: todo,
+          is_done: "false",
+        }),
+      })
+        .then((response) => response.json())
+        .then(() => {
+          setInputValue(""); 
+          return getTodos(); 
+        })
+        .catch((error) => console.log(error));
+    }
+    console.log("No se ha ingresado tarea");
   }
-
-
 
   useEffect(() => {
-    getUser()
-    getTodos()
-  }, [])
-
-
+    createUser();
+    getUser();
+    getTodos();
+  }, []);
 
   return (
     <div className="text-center toDoList">
-      <h1>To do List</h1>
-      {<h1>Usuario: {user.name}</h1>}
-      <input type="text" value={inputValue} onChange={(event) => setInputValue(event.target.value)} onKeyDown={(event) => event.key === 'Enter' && createTodo(inputValue)}
-        placeholder="¿Cual es la tarea a realizar?"
+      <h1>To Do List</h1>
+      <h2>Usuario: {user.name}</h2>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={(event) => event.key === 'Enter' && createTodo(inputValue)}
+        placeholder="¿Cuál es la tarea a realizar?"
       />
       <ul>
         {todos.length === 0 ? (
           <li className="task-item">No hay tareas registradas</li>
         ) : (
-          todos.map((item) => (
-            <li key={"id"} className="task-item">
+          todos.map((item, index) => (
+            <li key={index} className="task-item">
               {item.label}
-              <button className="delete-button" /*onClick={() => removeTask(index)}*/>X</button>
+              <button className="delete-button">X</button>
             </li>
           ))
         )}
       </ul>
     </div>
-
-  )
-
-}
+  );
+};
 
 export default ToDoList;
